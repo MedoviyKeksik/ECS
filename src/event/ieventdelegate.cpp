736f218ce0872c6335e5f5b60fd69bef8c0514a8
Vladislav Aleynikov <vladislav_v_01@mail.ru>
2022-02-01 23:04:09 +0300

    add clang-format

---
+++ src/event/ieventdelegate.cpp
@@ -0,0 +1,6 @@
+#include "ieventdelegate.h"
+
+IEventDelegate::IEventDelegate()
+{
+
+}
---
+++ src/event/ieventdelegate.h
@@ -0,0 +1,20 @@
+#ifndef IEVENTDELEGATE_H
+#define IEVENTDELEGATE_H
+
+#include "../util/global.hpp"
+
+namespace ecs
+{
+namespace event
+{
+
+}
+} // namespace ecs
+
+class IEventDelegate
+{
+public:
+    IEventDelegate();
+};
+
+#endif // IEVENTDELEGATE_H
